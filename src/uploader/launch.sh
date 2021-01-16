5f68f5291a68c55ea13bb454fddfdedf627eee1b
Ian Page Hands <iphands@gmail.com>
2021-01-16 14:00:50 -0600

    restart

--- src/uploader/launch.sh
+++ src/uploader/launch.sh
@@ -3,4 +3,4 @@ set -e
 mkdir -p secrets
 cp -r ../../secrets/imgur.sh ./secrets/
 docker build -t camserver .
-docker run --rm --name camserver -p 8000:8000 camserver
+docker run --restart always --rm --name camserver -p 8000:8000 camserver
